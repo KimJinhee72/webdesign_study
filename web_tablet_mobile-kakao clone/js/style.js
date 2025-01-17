d1705ee398f776c16d192936d50061da9cf8e944
KimJinhee72 <geenhankuk@gmail.com>
2025-01-17 18:24:39 +0900

    Add files via upload
    new까지

---
+++ web_tablet_mobile-kakao clone/js/style.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+  
+    const info = new Swiper(".info" , {
+        
+        slidesPerView: 3.5,
+        breakpoints: {
+            390: {
+                slidesPerView: 1.5,
+        },
+            768: {
+                slidesPerView: 4,
+
+        },
+            
+    }
+    }); 
+}
