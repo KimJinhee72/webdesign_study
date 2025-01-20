e4084287bfacd028a6ae69576f715abc0ea8fccf
KimJinhee72 <geenhankuk@gmail.com>
2025-01-20 15:53:21 +0900

    Add files via upload
    반응형 하는 중

--- web_tablet_mobile-kakao clone/js/style.js
+++ web_tablet_mobile-kakao clone/js/style.js
@@ -1,16 +1,1 @@
-window.onload = function () {
-    const info = new Swiper(".info" , {
-        
-        slidesPerView: 3.5,
-        breakpoints: {
-            390: {
-                slidesPerView: 1.5,
-        },
-            768: {
-                slidesPerView: 4,
-
-        },
-            
-    }
-    }); 
-}
+$(".header_bt_bmenu")
